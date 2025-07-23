a0237a1cf56b1d4fb4aa6b79524a969680bdb9f8
v.ionov <v.ionov@mycrg.ru>
2025-07-23 08:55:26 +0300

    init

--- src/main/java/com/example/jdbc/SqlRepository.java
+++ src/main/java/com/example/jdbc/SqlRepository.java
@@ -1,18 +1,37 @@
 package com.example.jdbc;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
+@RequiredArgsConstructor
 public class SqlRepository {
 
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     private final String sql = read("joinQueryMod.sql");
+
+    public String getProductName(String name) {
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        parameters.addValue("name", name);
+        
+        List<String> products = namedParameterJdbcTemplate.queryForList(sql, parameters, String.class);
+        
+        if (products.isEmpty()) {
+            return "Product not found for customer: " + name;
+        }
+        
+        return String.join(", ", products);
+    }
 
     private static String read(String scriptFileName) {
         try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
